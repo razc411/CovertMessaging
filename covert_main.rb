aad130df683b83698981b110f832b3fe4b5f890d
Ramzi Chennafi <razc411@hotmail.ca>
2015-09-18 15:32:59 -0700

    First Commit

---
+++ covert_main.rb
@@ -0,0 +1,19 @@
+# Covert UDP Messaging
+# Main Class
+# Uses UDP packet sizes to send covert messages between two clients.
+
+def send_cmsg
+
+end
+
+def recieve_cmsg
+
+end
+
+def intiate
+
+end
+
+def save_msg
+
+end
